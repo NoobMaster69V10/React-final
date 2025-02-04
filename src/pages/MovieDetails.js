5d0a1d13113e9d3f7fa1d1c51a39b604ddc78566
NoobMaster69V10 <giorgi.ubilava.1@btu.edu.ge>
2025-02-05 01:41:40 +0400

    Removed page

--- src/pages/MovieDetails.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-const MovieDetails = () => {
-    const { id } = useParams();
-    return <div>Movie Details for {id}</div>;
-};
-
-export default MovieDetails;
